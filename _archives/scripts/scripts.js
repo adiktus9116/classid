b5a72a3ba4a52f98d506b719e1cb3cdd5c9e2af0
unknown <Song@.(none)>
2014-01-11 00:23:14 +0800

    ver 0.01-20140110.01 - Initial software from Sir Ralph.

---
+++ _archives/scripts/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// $('.navigation').delegate('li','click',function(event){
+		// event.preventDefault();
+		// $('#body_container').html($(this).text());
+	// });
+});
